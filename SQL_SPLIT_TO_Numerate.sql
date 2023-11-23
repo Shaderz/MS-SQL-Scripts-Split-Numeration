6d419ccde577df23b0260bf3bfa6d3d141a5fbc7
SHD <75733440+Shaderz@users.noreply.github.com>
2023-11-23 12:42:18 +0300

    Create SQL_SPLIT_TO_Numerate.sql

---
+++ SQL_SPLIT_TO_Numerate.sql
@@ -0,0 +1,35 @@
+
+
+------------------------------------------------------------------------------------------------
+DECLARE @input NVARCHAR(MAX) = N'Lorem,ipsum,dolor,sit,amet,123,abc,000,555';
+declare @m int =1;
+
+WITH
+numbers AS (
+             SELECT n = 1, m=(select CHARINDEX(',', @input + ',', 1))-(select CHARINDEX(',', @input + ',', 1))
+             UNION ALL
+             SELECT n + 1, m +  ((select CHARINDEX(',', @input + ',', CHARINDEX(',', @input + ',', m)+1) )-(select CHARINDEX(',', @input + ',', m) ) )
+             FROM numbers
+             WHERE m <  LEN(@input+',')
+),
+
+parts  AS (
+             SELECT
+                    number = n
+                    ,m 
+                    ,part  = SUBSTRING(@input+ ',', m+1, CHARINDEX(',', @input+ ',,', m+1) - m-1)
+             FROM numbers
+)
+
+SELECT
+       number
+       ,part as strsplit
+       ,len(part) as lenstr
+       ,LEN(@input+',') as strlenght
+       ,m as delimpos
+       ,CHARINDEX(',', @input + ',', m+1) as nextpos
+
+FROM parts
+where  CHARINDEX(',', @input + ',', m+1) <>0
+OPTION (MAXRECURSION 1000);
+---------------------------------------------------------------------------------------------
